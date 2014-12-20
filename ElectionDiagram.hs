ddde6c40cdd413221b06afd534ec5c7d19d7e119
Stigni <davidegalliani@gmail.com>
2014-12-20 09:01:16 +0100

    New Version of ElectionDiagram

--- ElectionDiagram.hs
+++ ElectionDiagram.hs
@@ -3,62 +3,92 @@
 import Data.List
 import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
+import Data.Colour.SRGB
+import Data.Either
+
+
+------------ Dati Iniziali ----------
+type Seats = Int
+type Rows  = Int
+
+nArchi :: Rows
+nArchi = 9
+
+colourList :: [Colour Double]
+colourList = [(sRGB (0.5) (0.5) (0.5)), (sRGB (0.5+171/255/2) (0.5+205/255/2) (0.5+239/255/2)), (sRGB (0.5+30/255/2) (0.5+144/255/2) (255/255)),(sRGB (0.5+3/255/2) (0.5+192/255/2) (0.5+60/255/2)), (sRGB 0.7 0.7 0.7), (sRGB 0.7 0.7 0.7), (sRGB 0.7 0.7 0.7), (sRGB 1 1 (220/255)), (sRGB 1 0.5 0.5), (sRGB 1 0.5 0.5), (sRGB (0.5+123/255/2) (0.5+27/255/2) (0.5+2/255/2))]
+
+nSeats :: [Seats]
+nSeats = [16,12,127,28,5,4,15,10,1,70,12]
+
+---------- Main ed Elementi Grafici ----------
 
 main :: IO ()
-main = mainWith . pad 1.1 $ senate
-     
-senate :: Diagram B R2
-senate = mconcat . map (uncurry seatRow) 
-                 $ zip [23, 26, 28, 31, 34, 36, 39, 41, 44, 47]
-                       [1,1+1/9..]
+main = do
+    logo <- loadImageEmb "Logo.png"
+    mainWith . pad 1.1 $ (titolo <> sigle <> house <> filigrana <> image (head $ rights [logo]) # sized (Dims 0.8 0.8) # translateY (1/8) <> sottotitolo)
 
-seatRow :: Int -> Double -> Diagram B R2
-seatRow n r = centerX $ decorateTrail (seatRowTrail n r) (repeat node) 
+titolo :: Diagram B R2
+titolo = text "Greek Presidential Election 2014" # scale (1/5) # translateY (2.3)
 
-node :: Diagram B R2
-node = circle (2/45) # fc green
+sottotitolo :: Diagram B R2
+sottotitolo = text "First Vote 17 Dic 2014" # scale (1/8) # translateY 2.1
 
+sigle :: Diagram B R2
+sigle = text "MS" # scale ((radiusSeat nArchi (cumula nSeats))/2) # translateX 2
+
+filigrana :: Diagram B R2
+filigrana = text "scenaripolitici.com" # scale (1/6) # translateY (5/8) # opacity 0.7
+
+house :: Diagram B R2
+house = mconcat . map seatRow . addColourToSeat $ listaPallineArco nArchi (cumula nSeats)
+
+
+---------- Funzioni Grafiche per i Seggi del Parlamento -----------
+
+--Disegna una singola riga
+seatRow :: (Double,[(Seats, Colour Double)]) -> Diagram B R2
+seatRow (r,xs) = centerX $ decorateTrail (seatRowTrail (sum . map fst $ xs) r) (seatsList xs)
+
+--Assegna ai seggi di un determinato partito il suo colore
+addColourToSeat :: [(Double,[Seats])] -> [(Double,[(Seats,Colour Double)])]
+addColourToSeat ls = map (\(a,xs) -> (a,zip xs colourList)) ls
+
+--Crea la lista di tutti i seggi
+seatsList :: [(Seats,Colour Double)] -> [Diagram B R2]
+seatsList xs = mconcat . map (\(n,c)-> drawSeats (radiusSeat nArchi (cumula nSeats)) (take n (repeat c))) $ xs
+
+--Crea la primitiva dei seggi per ogni lista di colori
+drawSeats :: Double -> [Colour Double] -> [Diagram B R2]
+drawSeats r xc = map ( \a -> circle r # fc a) xc
+
+-- Calcola le primitive angolari per ogni riga del parlamento
 seatRowTrail :: (TrailLike t, V t ~ R2) => Int -> Double -> t
 seatRowTrail n r = polygon (with & polyType .~ PolyPolar (repeat angle)
                                                          (replicate n r))
     where angle = (1/(2*fromIntegral n) @@ turn)
                   
+--------------------- Funzioni Utili -----------------
 
------------------------------- GALLIANI ------------------------------
 
--- Dati iniziali
-type Seats = Int
-type Rows  = Int
-data Colore = Red | Yellow | Blue | Green
-
-nArchi :: Rows
-nArchi = 13
-
-partiti :: [(Seats,Colore)] 
-partiti = [(200, Red), (200, Yellow), (130, Blue), (100, Green)]
-
--- Raggi delle circonferenze
-radii :: Rows -> [Double]
-radii r = [1, 1+1/(fromIntegral r-1) .. 2]
-
--- Funzioni utili -- Non so come scriverle
+-- Funzione per cumulare una lista
 cumula :: [Seats]->[Seats]
 cumula = tail . map sum . inits
 
+-- Funzione per decumulare una lista
 decumula :: [Seats]->[Seats]
 decumula xs = head xs : zipWith (-) (tail xs) xs
 
---Decumulare lista (l'inversa)
--- Esempio
---Cumula . Map first $ partiti = [200,400,530,630]
 
-pallineCumulate :: [(Seats,Colore)] -> [Seats]
-pallineCumulate p = cumula . map fst $ p
+--------------------- Algoritmo per distribuire i seggi nelle varie file -------------------
+
+-- Raggi delle circonferenze
+radii :: Rows -> [Double]
+radii r = [1.0001, 1.0001+1/(fromIntegral r-1) .. 2.0001]
 
 -- Metodo d'Hondt (equivalente a Hagenbach-Bischoff)
 -- Calcolo la lista di tutti i coefficienti per la distribuzione
 listaCoeff :: Rows -> Seats -> [Double]
-listaCoeff r s = reverse . sort $ [i/j | i <- (radii r), j <- [1 .. 2 * s `div` r]]
+listaCoeff r s = reverse . sort $ [i/(fromIntegral j) | i <- (radii r), j <- [1 .. 2 * s `div` r]]
 
 -- Cerca il coefficiente per ogni partito
 hbCoefficients :: Rows -> [Seats] -> [Double]
@@ -71,8 +101,6 @@ listaPallineArcoCum r xs = map (\a -> (a, map (floor . (a/)) (hbCoefficients r x
 -- Decumula le palline
 listaPallineArco :: Rows -> [Seats] -> [(Double,[Seats])]
 listaPallineArco r xs = map (\(a,b) -> (a,decumula b)) (listaPallineArcoCum r xs)
-
--- Abbiamo una lista ad esempio di [(1,[20,20,13,10])..] dobbiamo passare a quella dei colori con 20 Rossi, 20 Gialli, 13 Blue e 10 Verdi
 
 -- Estrae il numero di palline complessive da un arco
 pallinePerArco :: [(Double,[Seats])] -> [(Double,Seats)]
@@ -87,5 +115,5 @@ distRadiale :: Rows -> Double
 distRadiale r = 1/(fromIntegral r-1)
 
 -- Determino i raggi delle palline, sugli archi tra due pallide ci deve essere una distanza radiale mentre sui Raggi principali almeno 0.01
-radius :: Rows -> [Seats] -> Double
-radius = minimum $ (map (/3) . distPerArco . pallinePerArco $ (listaPallineArcoCum r xs)) ++ [distRadiale r/2 - 0.01]
+radiusSeat :: Rows -> [Seats] -> Double
+radiusSeat r xs = minimum $ (map (/3) . distPerArco . pallinePerArco $ (listaPallineArcoCum r xs)) ++ [distRadiale r/2 - 0.01]
